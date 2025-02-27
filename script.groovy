595008be6a27ab5726037ea655dcbe064478200e
Atiq Ur Rehman <atiq_36@yahoo.com>
2025-02-27 21:03:20 +0500

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+  echo 'Building the application...'
+}
+
+def buildApp() {
+  echo 'Testing the application...'
+}
+
+def buildApp() {
+  echo 'Deploying the application...'
+  echo "deploying version ${params.VERSION}"
+}
+return this
