642f607cae1ffef1f29308109edfce750ca57b7a
Atiq Ur Rehman <atiq_36@yahoo.com>
2025-05-10 23:21:11 +0500

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,18 @@
+def buildJar() {
+  echo 'Building the application...'
+  sh 'mvn package'
+}
+
+def buildImage() {
+  echo 'Building the application...'
+  withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+    sh 'docker build -t 137.202.47.31:8083/jma_jenkinsfile:1.4 .'  
+    sh 'echo $PASS | docker login -u $USER --password-stdin 137.202.47.31:8083'
+    sh 'docker push 137.202.47.31:8083/jma_jenkinsfile:1.4'
+  }
+}
+
+def deployApp() {
+  echo 'Deploying the application...'
+}
+return this
