5c3d54cd25926c7f6064a6c7070d623da7081499
Atiq Ur Rehman <atiq_36@yahoo.com>
2025-05-19 12:26:33 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,11 +5,16 @@ def buildJar() {
 
 def buildImage() {
   echo 'Building the application...'
-  withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-    sh 'docker build -t 137.202.47.31:8083/jma_jenkinsfile:1.6 .'  
-    sh 'echo $PASS | docker login -u $USER --password-stdin 137.202.47.31:8083'
-    sh 'docker push 137.202.47.31:8083/jma_jenkinsfile:1.6'
+  withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+    sh 'docker build -t atiqghafoor/demo-app:jma-1.3 .'  
+    sh 'echo $PASS | docker login -u $USER --password-stdin'
+    sh 'docker push atiqghafoor/demo-app:1.2'
   }
+  //withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+  //  sh 'docker build -t 137.202.47.31:8083/jma_jenkinsfile:1.6 .'  
+ //   sh 'echo $PASS | docker login -u $USER --password-stdin 137.202.47.31:8083'
+ //   sh 'docker push 137.202.47.31:8083/jma_jenkinsfile:1.6'
+  //}
 }
 
 def deployApp() {
