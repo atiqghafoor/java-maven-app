cad7dd10e6f6adfa0b3ccf515c879c109ba1a0dd
Atiq Ur Rehman <atiq_36@yahoo.com>
2025-05-19 12:59:44 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
   echo 'Building the application...'
   withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-    sh 'docker build -t atiqghafoor/demo-app:jma-1.3 .'  
+    sh 'docker build -t atiqghafoor/demo-app:jma-1.4 .'  
     sh 'echo $PASS | docker login -u $USER --password-stdin'
-    sh 'docker push atiqghafoor/demo-app:jma-1.3'
+    sh 'docker push atiqghafoor/demo-app:jma-1.4'
   }
   //withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
   //  sh 'docker build -t 137.202.47.31:8083/jma_jenkinsfile:1.6 .'  
